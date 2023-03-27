6eaa589d8358beb71547549042d73a25d9714f1d
Fetth <kruglovegor0906@icloud.com>
2023-03-27 20:43:01 +0300

    DZ files

--- main.py
+++ main.py
@@ -1,66 +1,66 @@
-#1
-# f = open('file.txt', 'r', encoding='utf-8')
-# lines1 = set(f.read().split())
-# f.close()
-# f = open('file2.txt', 'r', encoding='utf-8')
-# lines2 = set(f.read().split())
-# f.close()
-#
-# print('Первое множество:', lines1)
-# print('Второе множество:', lines2)
-#
-# intersection = lines1.intersection(lines2)
-#
-# if len(intersection) > 0:
-#     print('Совпадения найдены:', intersection)
-# else:
-#     print('Совпадения не найдены')
-#
-#
-#3
+# 1
+f = open('file.txt', 'r', encoding='utf-8')
+lines1 = set(f.read().split())
+f.close()
+f = open('file2.txt', 'r', encoding='utf-8')
+lines2 = set(f.read().split())
+f.close()
 
-# fr = open('file_r.txt')
-# fw = open('file_w.txt', 'w')
-#
-# frl = fr.readlines()
-# frll = frl[:-1]
-#
-# for line in frll:
-#     fw.write(line)
-#
-# fr.close()
-# fw.close()
+print('Первое множество:', lines1)
+print('Второе множество:', lines2)
 
-#4
+intersection = lines1.intersection(lines2)
+
+if len(intersection) > 0:
+    print('Совпадения найдены:', intersection)
+else:
+    print('Совпадения не найдены')
 
 
-# f = open('file_r.txt')
-# text = f.read()
-# f.close()
-# lines = text.split('\n')
-# max_line = lines[0]
-# index = 0
-# for i in range(len(lines)):
-#     if len(max_line) < len(lines[i]):
-#         max_line = lines[i]
+# 3
+
+fr = open('file_r.txt')
+fw = open('file_w.txt', 'w')
+
+frl = fr.readlines()
+frll = frl[:-1]
+
+for line in frll:
+    fw.write(line)
+
+fr.close()
+fw.close()
+
+# 4
+
+
+f = open('file_r.txt')
+text = f.read()
+f.close()
+lines = text.split('\n')
+max_line = lines[0]
+index = 0
+for i in range(len(lines)):
+    if len(max_line) < len(lines[i]):
+        max_line = lines[i]
+
+print(max_line)
+
+# 5
+
+
+word = input("Введите слово ")
+count = 0
+with open("file5.txt", 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            if(i==word):
+                count=count+1
+print("Заданных слов",word, ":", count)
+
 #
-# print(max_line)
-
-#5
-
-
-# word = input("Введите слово ")
-# count = 0
-# with open("file5.txt", 'r') as f:
-#     for line in f:
-#         words = line.split()
-#         for i in words:
-#             if(i==word):
-#                 count=count+1
-# print("Заданных слов",word, ":", count)
-
-
-#6
+# 6
 
 search_text = input("Введите слово которое хотите найти ")
 replace_text = input("Введите на, что заменить ")
